ba4a0eb06e883c0a5ae2022f229e01f847ecb991
Monkeyhbd <monkeyhbd@163.com>
2024-06-11 01:09:46 +0800

    Add log mechanism.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,21 @@
 import jhtp
 import json
-# import traceback
 import _thread
+
+
+_LOG_PREFIX = '[DR2P]'
+_DEBUG_LEVEL = 0
+
+
+def set_debug_level(level):
+    global _DEBUG_LEVEL
+    _DEBUG_LEVEL = level
+
+
+def _log(text, level=1):
+    if level <= _DEBUG_LEVEL:
+        print(_LOG_PREFIX, end=' ')
+        print(text)
 
 
 class DR2PBase:
@@ -42,6 +56,8 @@ class DR2PPeer(DR2PBase):
             'version': '0'
         }
         body = json.dumps(msg).encode(encoding='utf-8')
+        _log('Send request head {}'.format(head))
+        _log('Send request body {}'.format(msg))
         self.j.send(head, body)
         self.callback_dict[rid] = callback
 
@@ -66,14 +82,18 @@ class DR2PPeer(DR2PBase):
 
         def routine(head, body):
             if head['type'] == 'request':
+                _log('Receive request head {}'.format(head))
                 path = head['path']
                 rid = head['id']
+                msg = json.loads(body.decode(encoding='utf-8'))  # JSON
+                _log('Receive request body {}'.format(msg))
+                _log('Calling handler...')
                 handler = self.handler_dict[path]()
                 handler.dr2p_peer = self
                 handler.head = head
                 handler.body = body
-                msg = json.loads(body.decode(encoding='utf-8'))  # JSON
                 res = handler.handle(msg)
+                _log('Handler returned.')
                 res_head = {
                     'type': 'response',
                     'code': 'OK',
@@ -81,12 +101,16 @@ class DR2PPeer(DR2PBase):
                     'version': '0'
                 }
                 res_body = json.dumps(res).encode(encoding='utf-8')  # JSON
+                _log('Send response head {}'.format(res_head))
+                _log('Send response body {}'.format(res))
                 self.j.send(res_head, res_body)
             elif head['type'] == 'response':
+                _log('Receive response head {}'.format(head))
+                msg = json.loads(body.decode(encoding='utf8'))
+                _log('Receive response body {}'.format(msg))
                 rid = head['id']
                 callback = self.callback_dict[rid]
-                callback.dr2p_peer = self
-                msg = json.loads(body.decode(encoding='utf8'))
+                callback.dr2p_peer = self  # ?
                 callback(
                     msg=msg,
                     head=head,
@@ -95,13 +119,14 @@ class DR2PPeer(DR2PBase):
 
         while self._mainloop_continue:
             try:
+                _log('Receive start.')
                 _head, _body = self.j.recv()
                 _thread.start_new_thread(routine, (_head, _body))
             except jhtp.TlaConnectionClose:
-                print('Peer closed connection.')
+                _log('Session closed.')
                 break
             except KeyboardInterrupt:
-                print('Keyboard interrupt, stop.')
+                _log('Keyboard interrupt, stop.')
                 break
 
 
